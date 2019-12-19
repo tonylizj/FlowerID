012cbf95a942a69b29a99321567f0c3178fdb99b
idonlakyo <daniel.lisq12@gmail.com>
2019-12-18 23:38:30 -0500

    Preliminary Model: ~0.25 accuracy

--- Main.py
+++ Main.py
@@ -4,18 +4,102 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import pandas as pd
-from sklearn.model.selection import train_test_split
+from PIL import Image
 
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-file_path = 'C:\Users\Daniel\Documents\GitHub\\flower-id\\flowers'
-_path = tf.keras.utils.get_file('flowers', origin=file_path, extract=False)
-PATH = os.path.join(os.path.dirname(_path), 'flowers')
-
-
+PATH = 'C:\\Users\\Daniel\\Documents\\GitHub\\flower-id\\nonResized'
 
 train_dir = os.path.join(PATH, 'train')
 validation_dir = os.path.join(PATH, 'validation')
+print(train_dir)
+
+batch_size = 128
+epochs = 5
+IMG_HEIGHT = 200
+IMG_WIDTH = 200
+total_train = 2850
+total_val = 1473
+
+train_image_generator = ImageDataGenerator(rescale=1./255,
+                                           rotation_range=45,
+                                           width_shift_range=.15,
+                                           height_shift_range=.15,
+                                           horizontal_flip=True,
+                                           zoom_range=0.5)
+validation_image_generator = ImageDataGenerator(rescale=1./255)
+
+train_data_gen = train_image_generator.flow_from_directory(batch_size=batch_size,
+                                                           directory=train_dir,
+                                                           shuffle=True,
+                                                           target_size=(IMG_HEIGHT, IMG_WIDTH),
+                                                           class_mode='binary')
+
+val_data_gen = validation_image_generator.flow_from_directory(batch_size=batch_size,
+                                                              directory=validation_dir,
+                                                              target_size=(IMG_HEIGHT, IMG_WIDTH),
+                                                              class_mode='binary')
+
+sample_training_images, _ = next(train_data_gen)
+
+
+def plotImages(images_arr):
+    fig, axes = plt.subplots(1, 5, figsize=(20,20))
+    axes = axes.flatten()
+    for img, ax in zip( images_arr, axes):
+        ax.imshow(img)
+        ax.axis('off')
+    plt.tight_layout()
+    plt.show()
+
+
+plotImages(sample_training_images[:5])
+
+model = Sequential([
+    Conv2D(16, 3, padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH ,3)),
+    MaxPooling2D(),
+    Conv2D(32, 3, padding='same', activation='relu'),
+    MaxPooling2D(),
+    Conv2D(64, 3, padding='same', activation='relu'),
+    MaxPooling2D(),
+    Flatten(),
+    Dense(512, activation='relu'),
+    Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit_generator(
+    train_data_gen,
+    steps_per_epoch=total_train // batch_size,
+    epochs=epochs,
+    validation_data=val_data_gen,
+    validation_steps=total_val // batch_size
+)
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
 
