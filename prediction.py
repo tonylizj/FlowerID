927d0dc005588c001957637c2f84e48e46a3306e
Tony Li <tonyli8101@gmail.com>
2019-12-20 03:58:11 -0500

    prints filenames now and supports multiple inputs

--- prediction.py
+++ prediction.py
@@ -11,12 +11,15 @@ input_dir = os.path.join(cwd, 'input')
 def predict(model, label_dir, input_dir):
     labels = [i for i in os.listdir(label_dir)]
     input_gen = ImageDataGenerator(rescale=1. / 255)
-    input_data_gen = input_gen.flow_from_directory(batch_size=128,
+    input_data_gen = input_gen.flow_from_directory(batch_size=1,
                                                    directory=input_dir,
                                                    target_size=(200, 200),
-                                                   class_mode='categorical')
+                                                   class_mode='categorical',
+                                                   shuffle=False)
 
-    print(labels[model.predict(input_data_gen).argmax(axis=-1)[0]])
+    predictions = [labels[j] for j in model.predict_generator(input_data_gen).argmax(axis=-1)]
+    print(predictions)
+    print(input_data_gen.filenames)
 
 
-predict(model, input_dir)
+predict(model,label_dir, input_dir)
